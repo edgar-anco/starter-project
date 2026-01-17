d80ee714f2ab9f9fa3167956fb83420a2c2906d0
Edgar Andre Anco Copaja <edanco1011@gmail.com>
2026-01-17 15:44:32 -0500

    feat(config): add PublishArticle route

--- frontend/lib/config/routes/routes.dart
+++ frontend/lib/config/routes/routes.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import '../../features/daily_news/domain/entities/article.dart';
 import '../../features/daily_news/presentation/pages/article_detail/article_detail.dart';
 import '../../features/daily_news/presentation/pages/home/daily_news.dart';
+import '../../features/daily_news/presentation/pages/publish_article/publish_article.dart';
 import '../../features/daily_news/presentation/pages/saved_article/saved_article.dart';
-
 
 class AppRoutes {
   static Route onGenerateRoutes(RouteSettings settings) {
@@ -17,7 +17,10 @@ class AppRoutes {
 
       case '/SavedArticles':
         return _materialRoute(const SavedArticles());
-        
+
+      case '/PublishArticle':
+        return _materialRoute(const PublishArticlePage());
+
       default:
         return _materialRoute(const DailyNews());
     }
