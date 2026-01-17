a7db47c69a85f2f8890dfce7f7054d5bb2fc416c
Edgar Andre Anco Copaja <edanco1011@gmail.com>
2026-01-17 15:15:51 -0500

    feat(data): add Firestore support to ArticleModel

--- frontend/lib/features/daily_news/data/models/article.dart
+++ frontend/lib/features/daily_news/data/models/article.dart
@@ -1,6 +1,7 @@
 import 'package:floor/floor.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
 import '../../../../core/constants/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 @Entity(tableName: 'article',primaryKeys: ['id'])
 class ArticleModel extends ArticleEntity {
@@ -14,16 +15,17 @@ class ArticleModel extends ArticleEntity {
     String ? publishedAt,
     String ? content,
   }): super(
-    id: id,
-    author: author,
-    title: title,
-    description: description,
-    url: url,
-    urlToImage: urlToImage,
-    publishedAt: publishedAt,
-    content: content,
-  );
+          id: id,
+          author: author,
+          title: title,
+          description: description,
+          url: url,
+          urlToImage: urlToImage,
+          publishedAt: publishedAt,
+          content: content,
+        );
 
+  /// Factory constructor for News API JSON
   factory ArticleModel.fromJson(Map < String, dynamic > map) {
     return ArticleModel(
       author: map['author'] ?? "",
@@ -36,6 +38,31 @@ class ArticleModel extends ArticleEntity {
     );
   }
 
+  /// Factory constructor for Firestore documents
+  factory ArticleModel.fromFirestore(Map<String, dynamic> map) {
+    String publishedAt = '';
+    if (map['publishedAt'] != null) {
+      if (map['publishedAt'] is Timestamp) {
+        publishedAt = (map['publishedAt'] as Timestamp).toDate().toIso8601String();
+      } else {
+        publishedAt = map['publishedAt'].toString();
+      }
+    }
+
+    return ArticleModel(
+      author: map['author'] ?? "",
+      title: map['title'] ?? "",
+      description: map['description'] ?? "",
+      url: map['url'] ?? "",
+      urlToImage: map['thumbnailUrl'] != null && map['thumbnailUrl'] != ""
+          ? map['thumbnailUrl']
+          : kDefaultImage,
+      publishedAt: publishedAt,
+      content: map['content'] ?? "",
+    );
+  }
+
+  /// Factory constructor from Entity
   factory ArticleModel.fromEntity(ArticleEntity entity) {
     return ArticleModel(
       id: entity.id,
@@ -47,5 +74,18 @@ class ArticleModel extends ArticleEntity {
       publishedAt: entity.publishedAt,
       content: entity.content
     );
+  }
+
+  /// Converts to Firestore document map
+  Map<String, dynamic> toFirestore() {
+    return {
+      'author': author,
+      'title': title,
+      'description': description,
+      'url': url,
+      'thumbnailUrl': urlToImage,
+      'publishedAt': publishedAt,
+      'content': content,
+    };
   }
 }
