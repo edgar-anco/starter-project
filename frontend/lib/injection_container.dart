ca25a39a4c9d7f66e6ec5578e143f7e99cce464a
Edgar Andre Anco Copaja <edanco1011@gmail.com>
2026-01-17 15:37:44 -0500

    feat(di): register Firestore dependencies and PublishArticleBloc

--- frontend/lib/injection_container.dart
+++ frontend/lib/injection_container.dart
@@ -1,10 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/firestore_service.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_firestore_articles.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/publish_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/publish/publish_article_bloc.dart';
 import 'features/daily_news/data/data_sources/local/app_database.dart';
 import 'features/daily_news/domain/usecases/get_saved_article.dart';
 import 'features/daily_news/domain/usecases/remove_article.dart';
@@ -14,46 +18,57 @@ import 'features/daily_news/presentation/bloc/article/local/local_article_bloc.d
 final sl = GetIt.instance;
 
 Future<void> initializeDependencies() async {
-
+  // Database
   final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
   sl.registerSingleton<AppDatabase>(database);
-  
+
   // Dio
   sl.registerSingleton<Dio>(Dio());
 
-  // Dependencies
+  // Data Sources
   sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+  sl.registerSingleton<FirestoreService>(FirestoreService());
 
+  // Repository
   sl.registerSingleton<ArticleRepository>(
-    ArticleRepositoryImpl(sl(),sl())
+    ArticleRepositoryImpl(sl(), sl(), sl()),
   );
-  
-  //UseCases
+
+  // Use Cases
   sl.registerSingleton<GetArticleUseCase>(
-    GetArticleUseCase(sl())
+    GetArticleUseCase(sl()),
+  );
+
+  sl.registerSingleton<GetFirestoreArticlesUseCase>(
+    GetFirestoreArticlesUseCase(sl()),
+  );
+
+  sl.registerSingleton<PublishArticleUseCase>(
+    PublishArticleUseCase(sl()),
   );
 
   sl.registerSingleton<GetSavedArticleUseCase>(
-    GetSavedArticleUseCase(sl())
+    GetSavedArticleUseCase(sl()),
   );
 
   sl.registerSingleton<SaveArticleUseCase>(
-    SaveArticleUseCase(sl())
+    SaveArticleUseCase(sl()),
   );
-  
+
   sl.registerSingleton<RemoveArticleUseCase>(
-    RemoveArticleUseCase(sl())
+    RemoveArticleUseCase(sl()),
   );
 
-
-  //Blocs
+  // Blocs
   sl.registerFactory<RemoteArticlesBloc>(
-    ()=> RemoteArticlesBloc(sl())
+    () => RemoteArticlesBloc(sl()),
   );
 
   sl.registerFactory<LocalArticleBloc>(
-    ()=> LocalArticleBloc(sl(),sl(),sl())
+    () => LocalArticleBloc(sl(), sl(), sl()),
   );
 
-
+  sl.registerFactory<PublishArticleBloc>(
+    () => PublishArticleBloc(sl()),
+  );
 }
